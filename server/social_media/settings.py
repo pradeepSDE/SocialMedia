a92d04c277ab24c5e1680b74ffea78f245e3b7e1
pradeepSDE <pradeepbisen60@gmail.com>
2025-04-30 19:58:50 +0530

    chore: update .gitignore to include environment variable files and modify settings.py for database configuration

--- server/social_media/settings.py
+++ server/social_media/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from datetime import timedelta
 from pathlib import Path
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,6 +87,16 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=os.environ.get('DATABASE_URL'),
+#         conn_max_age=600,
+#         ssl_require=True
+#     )
+# }
+
 
 
 # Password validation
