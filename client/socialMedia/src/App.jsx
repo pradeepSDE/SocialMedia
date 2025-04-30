41123c9d813b15c430007af4075cdbdefc801788
pradeepSDE <pradeepbisen60@gmail.com>
2025-04-30 19:31:41 +0530

    implement routing for authentication with login, signup, and dashboard views

--- client/socialMedia/src/App.jsx
+++ client/socialMedia/src/App.jsx
@@ -1,14 +1,34 @@
-import { useState } from 'react'
-import './App.css'
-import {Routes, Route} from 'react-router'
+import { Routes, Route, Navigate } from "react-router";
+import { useAuthStore } from "./store/authStore";
+import Login from "./components/auth/Login.jsx";
+import Signup from "./components/auth/Signup.jsx";
+import Dashboard from "./components/Dashboard.jsx";
+
 function App() {
+  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
 
   return (
-    <>
-
-      app
-    </>
-  )
+    <div className="app">
+      <Routes>
+        <Route
+          path="/login"
+          element={!isAuthenticated ? <Login /> : <Navigate to="/dashboard" />}
+        />
+        <Route
+          path="/signup"
+          element={!isAuthenticated ? <Signup /> : <Navigate to="/dashboard" />}
+        />
+        <Route
+          path="/dashboard"
+          element={isAuthenticated ? <Dashboard /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/"
+          element={<Navigate to={isAuthenticated ? "/dashboard" : "/login"} />}
+        />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
